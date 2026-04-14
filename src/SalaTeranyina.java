3f214791cf7fd7edb76fb45d9eeb88bd44756c99
Aleix Sales <aleix.salessegura2007@gmail.com>
2026-04-14 21:42:23 +0200

    Update SalaTeranyina.java

--- src/SalaTeranyina.java
+++ src/SalaTeranyina.java
@@ -17,18 +17,20 @@ public boolean intentarSortir(Personatge p) {
 		//el personaje hace una tirada con >--FUERZA--<.
 		
 		int tiradaForsa = numAleatoriTirada();
-		//si tiene exito --> TRUE
 		
+		//si tiene exito --> TRUE
+		System.out.println("  [Teranyina] Tirada de força: " + tiradaForsa + " (necessites <= " + p.forsa + ")");
+
 		if(tiradaForsa <=  p.forsa) {
+			System.out.println(" Has superat la teranyina!");
 			return true;
 		}
 		//si falla --> se queda en el mismo sitio y FALSE
 		else {
+			System.out.println(" No has superat la teranyina, et quedes atrapat!");
 			return false;
 		}
 //		Fin metodo
 	}
-
-
 
 }
